56ea8a36e27e7bf2caa009dc94548930320beda3
Ankit Mehrotra <gamerankitbuxar@gmail.com>
2020-08-22 17:34:03 +0530

    my first commit

---
+++ Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public void LoadOnPlayButton()
+    {
+        Debug.Log("Button Pressed");
+        SceneManager.LoadScene("ClassSelection");
+    }
+
+    public void Class12ButtonClicked()
+    {
+        SceneManager.LoadScene("GameChapters");
+    }
+    void Update()
+    {
+        
+    }
+}
