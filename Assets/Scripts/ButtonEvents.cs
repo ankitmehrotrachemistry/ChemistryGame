451030d1623c3b7410deaf1e18775d3b674f1112
Ankit Mehrotra <gamerankitbuxar@gmail.com>
2020-08-23 21:16:59 +0530

    New UI Changes

--- Assets/Scripts/ButtonEvents.cs
+++ Assets/Scripts/ButtonEvents.cs
@@ -5,15 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class ButtonEvents : MonoBehaviour
 {
+    public GameObject NotesText;
+
     public void LoadOnPlayButton()
     {
-        Debug.Log("Button Pressed");
         SceneManager.LoadScene("ClassSelection");
     }
 
+    public void NotesButton()
+    {
+        SceneManager.LoadScene("ClassSelection");
+        NotesText.SetActive(true);
+    }
+
+    public void ObjectiveBackButton()
+    {
+        SceneManager.LoadScene("ClassSelection");
+    }
+
+    public void SubjectiveBackButton()
+    {
+        SceneManager.LoadScene("ClassSelection");
+    }
+    public void BackButtonClick()
+    {
+        SceneManager.LoadScene("GameStart");
+    }
+
+    public void JEEButtonClick()
+    {
+        SceneManager.LoadScene("Objective");
+    }
+
+    public void NEETButtonClick()
+    {
+        SceneManager.LoadScene("Objective");
+    }
+
+    public void Class11ButtonClicked()
+    {
+        SceneManager.LoadScene("Subjective");
+    }
     public void Class12ButtonClicked()
     {
-        SceneManager.LoadScene("GameChapters");
+        SceneManager.LoadScene("Subjective");
     }
     void Update()
     {
